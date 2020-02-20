1ec8acf21b4147a68fda11bac3bd81de45551bf6
daniel fernandes <danielgiovane2015@gmail.com>
2020-02-20 13:14:59 -0300

    versao 0.0.3 gym-manager

--- routes.js
+++ routes.js
@@ -3,16 +3,32 @@ const express = require('express');
 const routes = express.Router();
 
 
-routes.get('/', (req,res) => {
+routes.get('/', (req, res) => {
   return res.redirect('/instructors');
 });
 
 
-routes.get('/instructors', (req,res) => {
+routes.get('/instructors', (req, res) => {
   return res.render('instructors/index');
 });
 
-routes.get('/members', (req,res) => {
+routes.get('/instructors/create', (req, res) => {
+  return res.render('instructors/create');
+});
+
+routes.post('/instructors', (req,res) => {
+  
+  const keys = Object.keys(req.body);
+
+  for(let key of keys){
+    if(req.body[key] == ""){
+      return res.send("por favor, preencha todos os campos.")
+    }
+  }
+  return res.send(req.body);
+});
+
+routes.get('/members', (req, res) => {
   return res.send('members');
 });
 
